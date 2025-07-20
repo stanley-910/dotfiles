117828f2dd3ea1448611c8e52a6f502f8630fda7
Stanley Wang <stanley.wang@autodesk.com>
2025-07-20 13:32:02 -0400

    july update

--- scripts/open_remote.sh
+++ scripts/open_remote.sh
@@ -5,23 +5,40 @@ dir=$(tmux display-message -p "#{pane_current_path}")
 cd "$dir"
 url=$(git remote get-url origin)
 url_branch=$(git branch --show-current)
+username='wangs3'
 
-# Check if the repository is on GitHub or Autodesk
-if [[ $url == *"github.com"* ]] || [[ $url == *"git.autodesk.com"* ]]; then
-  # Convert SSH URL to HTTPS if necessary
-  if [[ $url == git@* ]]; then
-    url=$(echo "$url" | sed 's/git@\(.*\):/https:\/\/\1\//')
-  fi
 
-  if [[ ! -z $url_branch ]]; then
-    if [[ $url_branch != "master" && $url_branch != "main" ]]; then
-      url=${url%.git} # rm git suffix if present
-      url="$url/tree/$url_branch"
+function main () {
+  # Check if the repository is on GitHub or Autodesk
+  if [[ $url == *"github.com"* ]] || [[ $url == *"git.autodesk.com"* ]]; then
+    # Convert SSH URL to HTTPS if necessary
+    if [[ $url == git@* ]]; then
+      url=$(echo "$url" | sed 's/git@\(.*\):/https:\/\/\1\//')
     fi
+
+    if [[ ! -z $url_branch ]]; then
+      if [[ $url_branch != "master" && $url_branch != "main" ]]; then
+        url=${url%.git} # rm git suffix if present
+        if [[ $1 == "h" ]]; then
+          url="$url/tree/$url_branch"
+        elif [[ $1 == "H" ]]; then
+          url="$url"
+        elif [[ $1 == "p" ]]; then
+          url="$url/pulls/$username"
+        else
+          echo "Invalid argument: $1"
+          exit 1
+        fi
+      fi
+    fi
+
+    open "$url"
+  else
+    echo "This repository is not hosted on GitHub or Autodesk Git"
+    exit 1
   fi
 
-  open "$url"
-else
-  echo "This repository is not hosted on GitHub or Autodesk Git"
-  exit 1
-fi
+}
+
+
+main "$@"
