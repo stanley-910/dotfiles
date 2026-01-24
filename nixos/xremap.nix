af7e98edcd1143b3542aa8b36714414a446ea2b2
Stanley Wang <wangstanley910@gmail.com>
2026-01-24 10:59:52 -0500

    generation 74

--- nixos/xremap.nix
+++ nixos/xremap.nix
@@ -60,8 +60,7 @@
           device:
             only: "Logitech USB Receiver"
           remap:
-            KEY_LEFTMETA: KEY_LEFTALT
-            KEY_LEFTALT: KEY_LEFTMETA
+            Shift_L: Esc
       
       keymap:
         - name: "Enter app launcher mode"
@@ -97,7 +96,7 @@
     environment = {
       # /run/current-system/sw/bin = system packages (environment.systemPackages)
       # /etc/profiles/per-user/<user>/bin = user-specific packages
-      PATH = lib.mkForce "/run/current-system/sw/bin:/etc/profiles/per-user/${config.services.xremap.userName}/bin:/home/${config.services.xremap.userName}/.nix-profile/bin";
+        PATH = lib.mkForce "/run/current-system/sw/bin:/etc/profiles/per-user/${config.services.xremap.userName}/bin:/home/${config.services.xremap.userName}/.nix-profile/bin";
       # Enable debug logging to see key events
       RUST_LOG = "debug";
     };
